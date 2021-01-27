30733cbf2863a01a0f24077a2f45255c5c8e3d18
galihdik <galihdikapermana@gmail.com>
2021-01-27 17:09:01 +0700

    update coursedaoimpl

--- features/src/main/java/com/lawencon/elearning/dao/impl/CourseDaoImpl.java
+++ features/src/main/java/com/lawencon/elearning/dao/impl/CourseDaoImpl.java
@@ -1,10 +1,17 @@
 package com.lawencon.elearning.dao.impl;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import com.lawencon.elearning.dao.CourseDao;
 import com.lawencon.elearning.dao.CustomBaseDao;
 import com.lawencon.elearning.model.Course;
+import com.lawencon.elearning.model.CourseCategory;
+import com.lawencon.elearning.model.CourseStatus;
+import com.lawencon.elearning.model.CourseType;
+import com.lawencon.elearning.model.Teacher;
+import com.lawencon.elearning.model.User;
 import com.lawencon.elearning.util.HibernateUtils;
 import com.lawencon.util.Callback;
 
@@ -48,10 +55,39 @@ public class CourseDaoImpl extends CustomBaseDao<Course> implements CourseDao {
             .toString();
     List<?> listObj = createNativeQuery(sql).getResultList();
 
-    List<Course> listResult = HibernateUtils.bMapperList(listObj, Course.class, "id", "code",
-        "courseType.name",
-        "capacity", "periodStart", "periodEnd", "teacher.id", "teacher.code", "user.firstName",
-        "user.lastName", "teacher.titleDegree", "category.code", "category.name");
+    List<Course> listResult = new ArrayList<>();
+    listObj.forEach(val -> {
+      Object[] objArr = (Object[]) val;
+      Course course = new Course();
+      course.setId((String) objArr[0]);
+      course.setCode((String) objArr[1]);
+
+      CourseType courseType = new CourseType();
+      courseType.setName((String) objArr[2]);
+      course.setCourseType(courseType);
+
+      course.setCapacity((Integer) objArr[3]);
+      course.setPeriodStart((LocalDateTime) objArr[4]);
+      course.setPeriodEnd((LocalDateTime) objArr[5]);
+
+      Teacher teacher = new Teacher();
+      teacher.setId((String) objArr[6]);
+      teacher.setCode((String) objArr[7]);
+      User user = new User();
+      user.setFirstName((String) objArr[8]);
+      user.setLastName((String) objArr[9]);
+      teacher.setUser(user);
+      teacher.setTitleDegree((String) objArr[10]);
+      course.setTeacher(teacher);
+
+      CourseCategory courseCategory = new CourseCategory();
+      courseCategory.setCode((String) objArr[11]);
+      courseCategory.setName((String) objArr[12]);
+      course.setCategory(courseCategory);
+
+      listResult.add(course);
+    });
+
     return listResult;
   }
 
@@ -66,12 +102,41 @@ public class CourseDaoImpl extends CustomBaseDao<Course> implements CourseDao {
         "INNER JOIN student_course AS sc ON c.id = sc.course_id ",
         "INNER JOIN tb_m_students AS s ON sc.student_id = s.id WHERE s.id = ?")
             .toString();
-
+    List<Course> listResult = new ArrayList<>();
     List<?> listObj = createNativeQuery(sql).setParameter(1, id).getResultList();
-    List<Course> listResult = HibernateUtils.bMapperList(listObj, Course.class, "id", "code",
-        "courseType.name",
-        "capacity", "periodStart", "periodEnd", "teacher.id", "teacher.code", "teacher.firstName",
-        "teacher.lastName", "teacher.titleDegree", "category.code", "category.name");
+    
+
+    listObj.forEach(val -> {
+      Object[] objArr = (Object[]) val;
+      Course course = new Course();
+      course.setId((String) objArr[0]);
+      course.setCode((String) objArr[1]);
+
+      CourseType courseType = new CourseType();
+      courseType.setName((String) objArr[2]);
+      course.setCourseType(courseType);
+
+      course.setCapacity((Integer) objArr[3]);
+      course.setPeriodStart((LocalDateTime) objArr[4]);
+      course.setPeriodEnd((LocalDateTime) objArr[5]);
+
+      Teacher teacher = new Teacher();
+      teacher.setId((String) objArr[6]);
+      teacher.setCode((String) objArr[7]);
+      User user = new User();
+      user.setFirstName((String) objArr[8]);
+      user.setLastName((String) objArr[9]);
+      teacher.setUser(user);
+      teacher.setTitleDegree((String) objArr[10]);
+      course.setTeacher(teacher);
+
+      CourseCategory courseCategory = new CourseCategory();
+      courseCategory.setCode((String) objArr[11]);
+      courseCategory.setName((String) objArr[12]);
+      course.setCategory(courseCategory);
+
+      listResult.add(course);
+    });
     return listResult;
   }
 
@@ -85,10 +150,11 @@ public class CourseDaoImpl extends CustomBaseDao<Course> implements CourseDao {
   @Override
   public List<Course> getCourseForAdmin() throws Exception {
     String sql = buildQueryOf(
-        "SELECT c.id ,c.code, ct.type_name AS courseName, c.capacity ,c.status ,c.descripton ,c.period_start ,c.period_end ,t.id ,t.code ,t.first_name ,t.last_name ,t.title_degree ,cc.code ,cc.category_name ",
+        "SELECT c.id ,c.code, ct.type_name AS courseName, c.capacity ,c.status ,c.descripton ,c.period_start ,c.period_end ,t.id ,t.code ,u.first_name ,u.last_name ,t.title_degree ,cc.code ,cc.category_name ",
         "FROM tb_m_courses AS c ",
         "INNER JOIN tb_m_course_types AS ct ON c.id_course_type = ct.id ",
         "INNER JOIN tb_m_teachers AS t ON c.id_teacher = t.id ",
+        "INNER JOIN tb_m_users AS u ON t.id_user = u.id ",
         "INNER JOIN tb_m_course_categories AS cc ON c.id_category = cc.id").toString();
 
     List<?> listObj = createNativeQuery(sql).getResultList();
@@ -97,6 +163,40 @@ public class CourseDaoImpl extends CustomBaseDao<Course> implements CourseDao {
         "capacity", "status", "description", "periodStart", "periodEnd", "teacher.id",
         "teacher.code", "teacher.firstName", "teacher.lastName", "teacher.titleDegree",
         "category.code", "category.name");
+
+    listObj.forEach(val -> {
+      Object[] objArr = (Object[]) val;
+      Course course = new Course();
+      course.setId((String) objArr[0]);
+      course.setCode((String) objArr[1]);
+
+      CourseType courseType = new CourseType();
+      courseType.setName((String) objArr[2]);
+      course.setCourseType(courseType);
+
+      course.setCapacity((Integer) objArr[3]);
+      course.setStatus(CourseStatus.valueOf((String) objArr[4]));
+      course.setDescripton((String) objArr[5]);
+      course.setPeriodStart((LocalDateTime) objArr[6]);
+      course.setPeriodEnd((LocalDateTime) objArr[7]);
+
+      Teacher teacher = new Teacher();
+      teacher.setId((String) objArr[8]);
+      teacher.setCode((String) objArr[9]);
+      User user = new User();
+      user.setFirstName((String) objArr[10]);
+      user.setLastName((String) objArr[11]);
+      teacher.setUser(user);
+      teacher.setTitleDegree((String) objArr[12]);
+      course.setTeacher(teacher);
+
+      CourseCategory courseCategory = new CourseCategory();
+      courseCategory.setCode((String) objArr[13]);
+      courseCategory.setName((String) objArr[14]);
+      course.setCategory(courseCategory);
+
+      listResult.add(course);
+    });
     return listResult;
   }
 
